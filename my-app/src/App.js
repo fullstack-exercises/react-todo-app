656fd910727cc625aa86de697b538c38397a947f
Jessica Michel <jessicamichel93@gmail.com>
2021-04-10 15:44:00 +0200

    changed component to React class based component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,35 +1,38 @@
+import React from "react";
 import "./App.css";
 import ToDoItem from "./components/ToDoItem/ToDoItem";
 import todosData from "./todosData";
 
-function App() {
-  const date = new Date();
-  const hours = date.getHours();
-  let timeOfDay;
-  const styles = {
-    color: "grey",
-  };
+class App extends React.Component {
+  render() {
+    const date = new Date();
+    const hours = date.getHours();
+    let timeOfDay;
+    const styles = {
+      color: "grey",
+    };
 
-  const todoItems = todosData.map((item) => (
-    <ToDoItem key="{item.id}" item={item} />
-  ));
+    const todoItems = todosData.map((item) => (
+      <ToDoItem key="{item.id}" item={item} />
+    ));
 
-  if (hours < 12) {
-    timeOfDay = "morning";
-  } else if (hours >= 12 && hours < 17) {
-    timeOfDay = "afternoon";
-  } else {
-    timeOfDay = "night";
+    if (hours < 12) {
+      timeOfDay = "morning";
+    } else if (hours >= 12 && hours < 17) {
+      timeOfDay = "afternoon";
+    } else {
+      timeOfDay = "night";
+    }
+
+    return (
+      <div className="App">
+        <header className="Header">
+          <h1 style={styles}>Good {timeOfDay}!</h1>
+        </header>
+        <main>{todoItems}</main>
+      </div>
+    );
   }
-
-  return (
-    <div className="App">
-      <header className="Header">
-        <h1 style={styles}>Good {timeOfDay}!</h1>
-      </header>
-      <main>{todoItems}</main>
-    </div>
-  );
 }
 
 export default App;
